c6ab02c6e2b737d19ee8ce6469d1f2e94af53111
Your Name <wrnermontala@gmail.com>
2022-10-14 03:40:11 +0800

    added userloggedin and userslist on localStorage, to fix login even if wrong pw

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
+  //list of all users
+  const headers = JSON.parse(localStorage.getItem("loginCredentials")).headers;
+  fetch("http://206.189.91.54/api/v1/users", {
+    method: "GET",
+    headers,
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 
   return (
     <div>
-      CHANNELS<br/>
+      <div>
+        <h2>Channels</h2>
+        <p>Create new channel</p>
+      </div>
       CHAT
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,40 +1,72 @@
-import React from 'react'
+import React from "react";
 
-const SignIn = ({successSignIn}) => {
+const SignIn = ({ successSignIn }) => {
+  // const signIn = (e) => {
+  //   e.preventDefault();
+  //   fetch("http://206.189.91.54/api/v1/auth/sign_in", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify({
+  //       "email": e.target.signInEmail.value,
+  //       "password": e.target.signInPassword.value,
+  //     })
+  //   }).then(res => {
+  //     return res.json()
+  //   })
+  //  // .then(data => success(data))
+  //   .catch(error => console.log("error sign in"))
+  // }
 
-  const signIn = (e) => {
+  const signIn = async (e) => {
     e.preventDefault();
-    fetch("http://206.189.91.54/api/v1/auth/sign_in", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        "email": e.target.signInEmail.value,
-        "password": e.target.signInPassword.value,
-      })
-    }).then(res => {
-      return res.json()
-    })
-    .then(data => success(data))
-    .catch(error => console.log("error sign in"))
-  }
+    let errors;
+    let data;
+
+    try {
+      const res = await fetch("http://206.189.91.54/api/v1/auth/sign_in", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: e.target.signInEmail.value,
+          password: e.target.signInPassword.value,
+        }),
+      });
+      const body = await res.json();
+      const headers = [...res.headers.entries()].reduce((acc, val) => {
+        acc[val[0]] = val[1];
+        return acc;
+      }, {});
+
+      data = { body, headers };
+      success(data);
+      localStorage.setItem("loginCredentials", JSON.stringify(data));
+    } catch (error) {
+      errors = error.errors;
+      console.log(errors);
+    }
+  };
 
   const success = (data) => {
-    console.log(data);
+    //console.log(data);
     successSignIn();
-  }
+  };
 
   return (
     <div>
       <h2>Sign In</h2>
       <form onSubmit={signIn}>
-        <input name="signInEmail" type="email" placeholder="Email"/><br/>
-        <input name="signInPassword" type="password" placeholder="Password"/><br/>
+        <input name="signInEmail" type="email" placeholder="Email" />
+        <br />
+        <input name="signInPassword" type="password" placeholder="Password" />
+        <br />
         <button type="submit">Sign In</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
