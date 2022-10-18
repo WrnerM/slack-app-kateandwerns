4fee13b62d1907af9edfc4470add24bb3df3e50d
Kate Pelinggon <katepelinggon@gmail.com>
2022-10-18 22:35:25 +0800

    add headers for send message

--- src/components/Messaging.jsx
+++ src/components/Messaging.jsx
@@ -1,41 +1,50 @@
 import React from 'react'
 
-const Messaging = ({loginCredentials}) => {
+const Messaging = () => {
+
+  // Get logged in user's credentials
+  const loginCredentials = JSON.parse(localStorage.getItem("loginCredentials"));
+  const headers = loginCredentials.headers;
 
   const searchUser = (e) => {
 
   }
 
-  const retrieveMessage = (e) => {
-    e.preventDefault();
+  // const retrieveMessage = async (e) => {
+  //   e.preventDefault();
 
-      try{
-        // Fetch Avion API
-        const res = await fetch("http://206.189.91.54/api/v1/messages?receiver_id=1&receiver_class=User",{
-          method: "GET",
-          headers: {
-            "access-token":
-            client:
-            expiry:
-            uid:
-          }
-        })
-          .then((res) => res.json())
+  //     try{
+  //       // Fetch Avion API
+  //       const res = await fetch("http://206.189.91.54/api/v1/messages?receiver_id=1&receiver_class=User",{
+  //         method: "GET",
+  //         headers: {
+  //           "access-token":
+  //           client:
+  //           expiry:
+  //           uid:
+  //         }
+  //       })
+  //         .then((res) => res.json())
   
-          // Show data if fetch is successful
-          .then((data) => {
-            console.log(data)
-          })
+  //         // Show data if fetch is successful
+  //         .then((data) => {
+  //           console.log(data)
+  //         })
   
-        // Show error if fetch is unsuccessful
-      } catch (error) {
-        console.log(error);
-      }
-  };
+  //       // Show error if fetch is unsuccessful
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  // };
 
   const sendMessage = async (e) => {
     e.preventDefault();
-    // let receiverID = data.map(x => x.id);
+    // let receiverID = headers.map(x => x.uid);
+
+    let accessToken = headers["access-token"];
+    let clientData = headers["client"];
+    let expiryData = headers["expiry"];
+    let uidData = headers["uid"];
 
       try{
         // Fetch Avion API
@@ -43,8 +52,15 @@ const Messaging = ({loginCredentials}) => {
           method: "POST",
           body: {
             // "receiver_id": receiverID,
+            // "receiver_id": 2787,
             "receiver_class": "User",
-            "body": e.target.sendMessage.value
+            body: e.target.messageBox.value
+          },
+          headers: {
+            "access-token": accessToken,
+            client: clientData,
+            expiry: expiryData,
+            uid: uidData,
           }
         })
           .then((res) => res.json())
@@ -64,15 +80,15 @@ const Messaging = ({loginCredentials}) => {
     <div>
       <div>
         <input name="searchBar" type="text" placeholder="Search User" />
-        <button id="searchUser" onClick={searchUser}>+</button>
+        <button onClick={searchUser}>+</button>
       </div>
       <div>
-        square showing messages
+        square showing messages<br/><br/><br/><br/><br/>square showing messages
       </div>
-      <div>
+      <form onSubmit={sendMessage}>
         <input name="messageBox" type="text" placeholder="Type message..."/>
-        <button id="sendMessage" onClick={sendMessage}>Send message</button>
-      </div>
+        <button type="submit">Send message</button>
+      </form>
     </div>
   )
 }
