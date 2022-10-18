37605a7a22ac8f8e7448e15483c900c7a69666a7
Kate Pelinggon <katepelinggon@gmail.com>
2022-10-18 19:52:02 +0800

    refactor fetch all users api

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,25 +1,42 @@
 import React from "react";
+import Messaging from "./Messaging";
 
 const Dashboard = () => {
-  //list of all users
-  // const headers = JSON.parse(localStorage.getItem("loginCredentials")).headers;
-  // console.log(headers)
-  // fetch("http://206.189.91.54/api/v1/users", {
-  //   method: "GET",
-  //   headers,
-  // })
-  //   .then((res) => res.json())
-  //   .then((data) => console.log(data));
 
-  
+  // Get headers of logged in user
+  const headers = JSON.parse(localStorage.getItem("loginCredentials")).headers;
+  console.log(headers)
+
+  // Get list of ALL USERS
+  const getUsers = async (e) => {
+    e.preventDefault();
+    try{
+      // Fetch Avion API
+      const res = await fetch("http://206.189.91.54/api/v1/users",{
+        method: "GET",
+        headers: headers
+      })
+        .then((res) => res.json())
+
+        // Show data if fetch is successful
+        .then((data) => {
+          console.log(data)
+        })
+
+      // Show error if fetch is unsuccessful
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
+      <button onClick={getUsers}>Get Users</button>
       <div>
         <h2>Channels</h2>
         <p>Create new channel</p>
       </div>
-      CHAT
+        <Messaging />
     </div>
   );
 };
