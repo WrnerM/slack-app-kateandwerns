5061e069843ba7f648b18aa3f403611d2b26eb85
Your Name <wrnermontala@gmail.com>
2022-10-19 14:01:22 +0800

    added useEffect for single call per inputs

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import './App.css';
-import Authentication from './components/Authentication';
-import Dashboard from './components/Dashboard';
+import "./App.css";
+import Authentication from "./components/Authentication";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   // Stay logged in
   const loggedIn = localStorage.getItem("isLoggedIn");
 
-  return (
-    <div>
-      {loggedIn
-        ? <Dashboard />
-        : <Authentication />
-      }
-    </div>
-  );
+  return <div>{loggedIn ? <Dashboard /> : <Authentication />}</div>;
 }
 
 export default App;
--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,31 +1,30 @@
-import React from 'react'
-import { useState } from 'react'
-import SignUp from './SignUp'
-import SignIn from './SignIn'
+import React from "react";
+import { useState } from "react";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
 
 const Authentication = () => {
-
   const [showSignUp, setShowSignUp] = useState(false);
   const [showSignIn, setShowSignIn] = useState(true);
 
   const toggleSignIn = () => {
     setShowSignUp(false);
     setShowSignIn(true);
-  }
+  };
 
   const toggleSignUp = () => {
     setShowSignUp(true);
     setShowSignIn(false);
-  }
+  };
 
   return (
     <div>
       <button onClick={toggleSignIn}>Sign In</button>
       <button onClick={toggleSignUp}>Sign Up</button>
-      {showSignUp && <SignUp toggleSignIn={toggleSignIn}/>}
-      {showSignIn && <SignIn/>}
+      {showSignUp && <SignUp toggleSignIn={toggleSignIn} />}
+      {showSignIn && <SignIn />}
     </div>
-  )
-}
+  );
+};
 
-export default Authentication
+export default Authentication;
